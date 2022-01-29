f8348abaa45fc6d6bf0cebc83ff448f73f3edcb3
Sameer Ahmed <sameer7483@gmail.com>
2022-01-29 09:55:31 -0500

    Time: 253 ms (28.99%), Space: 41.8 MB (7.59%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -0,0 +1,33 @@
+class Solution {
+     boolean[] selected;
+    public boolean makesquare(int[] m) {
+        int sum = Arrays.stream(m).sum();
+        selected = new boolean[m.length+1];
+        if(sum % 4 != 0)
+            return false;
+        return backtrack(m, 0, 4, sum/4,sum/4);
+    }
+    
+    public boolean backtrack(int[] nums, int start, int k, int currSum, int sum){  
+        if(currSum < 0)
+            return false;
+        if(k == 0)
+            return true;
+        if(currSum == 0){
+            return backtrack(nums, 0, k-1, sum , sum);
+        }
+        if(start == nums.length)
+            return false;
+        
+        for(int i=start;i<nums.length;i++){
+            if(!selected[i]){
+                selected[i] = true;
+                if(backtrack(nums, i+1, k, currSum-nums[i], sum))
+                    return true;   
+                selected[i] = false;
+            }
+
+        }
+        return false;
+    }
+}
