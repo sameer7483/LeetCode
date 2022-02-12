c5422196b649a7994361df52d1bd6ddcb14fba2e
Sameer Ahmed <sameer7483@gmail.com>
2022-02-12 15:11:22 -0500

    Time: 2 ms (23.90%), Space: 43.3 MB (5.62%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.java
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -1,5 +1,55 @@
+class TrieNode{
+    Map<Character, TrieNode> children;
+    boolean isEnd;
+    public TrieNode(){
+        this.children = new HashMap<>();
+        this.isEnd = false;
+        
+    }
+}
+class Trie{
+    TrieNode root;
+    public Trie(){
+        this.root = new TrieNode();
+    }
+    
+    public void insert(String word){
+        TrieNode ws = root;
+        for(int i=0;i<word.length();i++){
+            if(!ws.children.containsKey(word.charAt(i))){
+                ws.children.put(word.charAt(i), new TrieNode());
+            }
+            ws = ws.children.get(word.charAt(i));  
+        }
+        ws.isEnd = true;
+    }
+    
+    public boolean search(String word){
+        TrieNode ws = root;
+        for(int i=0;i<word.length();i++){
+            if(!ws.children.containsKey(word.charAt(i)) && Character.isUpperCase(word.charAt(i))) return false;
+            if(ws.children.containsKey(word.charAt(i)))
+                ws = ws.children.get(word.charAt(i));  
+        }
+        if(ws.isEnd)
+            return true;
+        return false;
+    }    
+}
+
 class Solution {
     public List<Boolean> camelMatch(String[] queries, String p) {
+        Trie trie = new Trie();
+        trie.insert(p);
+        List<Boolean> res = new ArrayList<>();
+        for(String word : queries){
+            res.add(trie.search(word));
+        }
+        return res;
+    }
+    
+    
+    public List<Boolean> traversal(String[] queries, String p){
         List<Boolean> res = new ArrayList<>();
     
         for(String str : queries){
@@ -22,6 +72,6 @@ class Solution {
                 
         }
         return res;
-        
+                
     }
 }
