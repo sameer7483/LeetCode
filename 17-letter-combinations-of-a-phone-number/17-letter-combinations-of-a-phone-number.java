b46c930788f53cb9f5ff167612a57c06f0614f80
Sameer Ahmed <sameer7483@gmail.com>
2022-02-14 16:28:06 -0500

    Time: 2 ms (62.94%), Space: 42.1 MB (24.12%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    Map<Character, List<String>> m;
+    public List<String> letterCombinations(String digits) {
+        m = new HashMap<>();
+        m.put('2', Arrays.asList("a", "b", "c"));
+        m.put('3', Arrays.asList("d", "e", "f"));
+        m.put('4', Arrays.asList("g", "h", "i"));
+        m.put('5', Arrays.asList("j", "k", "l"));
+        m.put('6', Arrays.asList("m", "n", "o"));
+        m.put('7', Arrays.asList("p", "q", "r", "s"));
+        m.put('8', Arrays.asList("t", "u", "v"));
+        m.put('9', Arrays.asList("w", "x", "y", "z"));
+        List<String> res = new ArrayList<>();
+        backtrack(digits, 0, "", res);
+        return res;
+    }
+    
+    public void backtrack(String digits,int start, String path, List<String> res){
+        if(start == digits.length()){
+            if(!path.isEmpty())
+                res.add(path);
+            return;
+        }
+        List<String> l = m.get(digits.charAt(start));
+        for(int i=0;i<l.size();i++){
+            backtrack(digits, start+1, path+l.get(i), res);
+        }
+    }
+    
+
+}
