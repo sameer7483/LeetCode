5967fd1f039d04433c6f6bb876c2a659b385f7c2
Sameer Ahmed <sameer7483@gmail.com>
2022-02-15 09:28:49 -0500

    Time: 63 ms (18.29%), Space: 72.7 MB (7.61%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -4,7 +4,7 @@ class Solution {
         dp = new long[coins.length+1][amount+1];
         //dp[i][j]= min coin needed to make j amount using first i coins.
         int n = coins.length;
-        Arrays.fill(dp[0], Long.MAX_VALUE/2);
+        Arrays.fill(dp[0], amount+1);
         dp[0][0] = 0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
@@ -18,7 +18,7 @@ class Solution {
             
         }
         
-        if(dp[n][amount] >= Integer.MAX_VALUE)
+        if(dp[n][amount] > amount)
             return -1;
         return (int)dp[n][amount];
     }
