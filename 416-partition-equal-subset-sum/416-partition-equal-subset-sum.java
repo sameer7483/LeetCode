65ed1eeaae5e239b06d925503543df8df326b561
Sameer Ahmed <sameer7483@gmail.com>
2022-02-16 11:03:36 -0500

    Time: 93 ms (27.96%), Space: 54 MB (36.78%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -7,16 +7,15 @@ class Solution {
         int target = sum/2;
         //dp[i][j] is can we get j using i element;
         boolean dp[][] = new boolean[n+1][target+1];
-        for(int i=0;i<=n;i++)
-            dp[i][0] = true;
+        for(int i=0;i<=n;i++){
+            dp[i][0] =true;
+        }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=target;j++){
-                if(j>=nums[i-1]){
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                else{
+                if(j-nums[i-1] >=0)
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else
                     dp[i][j] = dp[i-1][j];
-                }
             }
         }
         return dp[n][target];
