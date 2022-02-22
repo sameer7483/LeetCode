210751c872f5967059c059f3a9b80b3285ba94a0
Sameer Ahmed <sameer7483@gmail.com>
2022-02-22 09:08:48 -0500

    Time: 2 ms (59.80%), Space: 41.4 MB (19.59%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int ans = 1;
+        for(int i=0;i<=30;i++){
+            if(n == ans)
+                return true;
+            ans = ans *2;
+        }
+        return false;
+    }
+}
