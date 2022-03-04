a65284fea962b58275e4458b8c284c589e2bda75
Sameer Ahmed <sameer7483@gmail.com>
2022-03-04 09:14:51 -0500

    Time: 76 ms (23.67%), Space: 43.8 MB (19.98%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        long[][] dp = new long[d+1][target+1];
+        for(int i=0;i<=d;i++)
+            dp[i][0] = 0;
+        for(int i=0;i<=target;i++)
+            dp[0][i] = 0;
+        dp[0][0] = 1;
+        int count = 0;
+        int mod = (int)(1e9+7);
+
+        for(int i=1;i<=d;i++){
+            for(int k=1;k<=f;k++){
+                for(int j=1;j<=target;j++){
+                    if(j >= k)
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-k])%mod;
+                }                            
+                }
+
+            }
+            // count = (count+numRollsToTarget(d-1, f, target-i))%mod;
+        return (int) dp[d][target]; 
+    }
+    
+    
+}
