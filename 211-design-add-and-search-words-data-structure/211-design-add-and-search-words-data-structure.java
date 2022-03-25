c206dbd591f35c958c068a0bede27cea775eec6f
Sameer Ahmed <sameer7483@gmail.com>
2022-03-25 11:17:01 -0400

    Time: 927 ms (19.85%), Space: 239.2 MB (24.95%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -1,71 +1,52 @@
 class TrieNode{
-    TrieNode[] children;
+    TrieNode children[];
     boolean isEnd;
     public TrieNode(){
         children = new TrieNode[26];
+        isEnd = false;
     }
+    
 }
 
-class Trie{
+class WordDictionary {
     TrieNode root;
-    public Trie(){
+    public WordDictionary() {
         root = new TrieNode();
     }
     
-    public void insert(String word){
+    public void addWord(String word) {
         TrieNode ws = root;
         for(int i=0;i<word.length();i++){
-            if(ws.children[word.charAt(i)-'a'] == null){
+            if(ws.children[word.charAt(i)-'a'] == null)
                 ws.children[word.charAt(i)-'a'] = new TrieNode();
-            }
-            ws = ws.children[word.charAt(i)-'a']; 
+            ws = ws.children[word.charAt(i)-'a'];
         }
         ws.isEnd = true;
     }
     
-    public boolean search(String word){
-        TrieNode ws = root;
-        return util(ws, word);
-              
+    public boolean search(String word) {
+       return searchUtil(word, 0, root);
+            
     }
-    public boolean util(TrieNode ws, String word){
-        if(ws == null)
+    public boolean searchUtil(String word,int start, TrieNode r){
+        if(r == null)
             return false;
-        if(ws.isEnd && (word == null || word.isEmpty()))
+        if(start == word.length() && r.isEnd)
             return true;
-        if(word.isEmpty())
+        if(start == word.length())
             return false;
-        if(word.charAt(0) != '.'){
-            if(ws.children[word.charAt(0)-'a'] == null)
-                return false;
-            return util(ws.children[word.charAt(0)-'a'], word.substring(1));
+
+        if(word.charAt(start) != '.'){
+            return searchUtil(word, start+1, r.children[word.charAt(start)-'a']);
         }
         else{
-            boolean flag = false;
             for(int i=0;i<26;i++){
-                if(ws.children[i] != null){
-                    flag = flag || util(ws.children[i], word.substring(1));
+                if(searchUtil(word, start+1, r.children[i])){
+                    return true;
                 }
-                if(flag) return true;
             }
             return false;
         }
-    }
-}
-
-
-class WordDictionary {
-    Trie trie;
-    public WordDictionary() {
-        trie = new Trie();
-    }
-    
-    public void addWord(String word) {
-        trie.insert(word);
-    }
-    
-    public boolean search(String word) {
-        return trie.search(word);
     }
 }
 
