071520ba1f225a450c262f6f1eccc5d7a38022a8
Sameer Ahmed <sameer7483@gmail.com>
2022-03-26 11:58:17 -0400

    Time: 4 ms (79.36%), Space: 45.4 MB (24.71%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,19 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
-            return false;
-        int n = s.length();
-        int[] count = new int[26];
-        Arrays.fill(count, 0);
-        for(int i=0;i<n;i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            
-            if(count[i] != 0)
-                return false;
-        }
-        return true;
+        char[] schar = s.toCharArray();
+        Arrays.sort(schar);
+        char[] tchar = t.toCharArray();
+        Arrays.sort(tchar);
+       return  String.valueOf(schar).equals(String.valueOf(tchar));
     }
 }
