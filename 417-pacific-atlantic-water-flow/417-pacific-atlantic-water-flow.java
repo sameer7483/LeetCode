f401a772c6bc91f177411e18932f7bb4d6f07f12
Sameer Ahmed <sameer7483@gmail.com>
2022-03-27 08:45:13 -0400

    Time: 39 ms (17.00%), Space: 73.3 MB (7.92%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,37 +1,36 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> p = new HashSet<>();
+        Set<List<Integer>> a = new HashSet<>();
         int m = heights.length;
-        int n = heights[0].length;   
-        Set<List<Integer>> pac = new HashSet<>();
-        Set<List<Integer>> atl = new HashSet<>();
-        for(int c=0;c<n;c++){
-            dfs(heights, 0, c, pac, heights[0][c]);
-            dfs(heights, m-1, c, atl, heights[m-1][c]);
+        int n = heights[0].length;
+        for(int i=0;i<m;i++){
+            dfs(heights, i,0,p,0);
+            dfs(heights, i, n-1,a, 0);
         }
-        for(int r=0;r<m;r++){
-            dfs(heights, r, 0, pac, heights[r][0]);
-            dfs(heights, r, n-1, atl, heights[r][n-1]);
-        }    
-        for(int i=0; i<m;i++){
-            for(int j=0;j<n;j++){
-                if(pac.contains(Arrays.asList(i, j)) && atl.contains(Arrays.asList(i, j)))
-                    res.add(Arrays.asList(i, j));
+        for(int i=0;i<n;i++){
+            dfs(heights, 0,i,p,0);
+            dfs(heights, m-1, i,a, 0);
+        }  
+        List<List<Integer>> res = new ArrayList<>();
+        for(List<Integer> l : p){
+            if(a.contains(l)){
+                res.add(l);
             }
+   
         }
         return res;
     }
+
     
-    public void dfs(int[][] heights, int i, int j, Set<List<Integer>> s, int prevHeight){
+    public void dfs(int[][] heights, int r, int c, Set<List<Integer>> s, int prevHeight){
         
-        int m = heights.length;
-        int n = heights[0].length;
-        if(i >=m || i<0 || j >= n || j< 0 || s.contains(Arrays.asList(i,j)) || heights[i][j] < prevHeight)
-            return ;
-        s.add(Arrays.asList(i,j));
-        dfs(heights, i+1, j, s, heights[i][j]);
-        dfs(heights, i-1, j, s, heights[i][j]);
-        dfs(heights, i, j+1, s, heights[i][j]);
-        dfs(heights, i, j-1, s, heights[i][j]);  
+        if(r < 0 || r >= heights.length || c < 0 || c >= heights[0].length || s.contains(Arrays.asList(r,c)) || heights[r][c] < prevHeight)
+            return;
+        s.add(Arrays.asList(r,c));
+        dfs(heights, r+1, c, s, heights[r][c]);
+        dfs(heights, r-1, c, s, heights[r][c]);
+        dfs(heights, r, c+1, s, heights[r][c]);
+        dfs(heights, r, c-1, s, heights[r][c]);
     }
 }
