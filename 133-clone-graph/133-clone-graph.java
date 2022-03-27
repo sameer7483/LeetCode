aa68572d76979eeac0f899bfd9fcb0649e20880c
Sameer Ahmed <sameer7483@gmail.com>
2022-03-27 09:32:23 -0400

    Time: 22 ms (90.40%), Space: 42.1 MB (92.32%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -30,36 +30,35 @@ class Pair{
 class Solution {
     public Node cloneGraph(Node node) {
         if(node == null)
-            return null;
-      Node newNode = new Node(node.val);
+            return node;
+        Node newNode = new Node(node.val);
+        Set<Integer> vis = new HashSet<>();
         Map<Integer, Node> m = new HashMap<>();
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(node, newNode));
         m.put(newNode.val, newNode);
-        Map<Integer, Boolean> v = new HashMap<>();
-        v.put(node.val, true);
+        Queue<Node> q = new LinkedList<>();
+        vis.add(node.val);
+        q.add(node);
         while(!q.isEmpty()){
-            Pair p = q.poll();
-            v.put(p.n1.val, true);
-            ArrayList<Node> l = new ArrayList<>();
-            for(int i=0;i<p.n1.neighbors.size();i++){
-                if(m.containsKey(p.n1.neighbors.get(i).val)){
-                    l.add(m.get(p.n1.neighbors.get(i).val));
+            int size = q.size();
+            for(int i=0;i<q.size();i++){
+                Node p = q.poll();
+                List<Node> l = new ArrayList<>();
+                for(Node n : p.neighbors){
+                        Node newN = m.getOrDefault(n.val, new Node(n.val));
+                        l.add(newN);
+                        m.put(newN.val, newN); 
+                        if(!vis.contains(n.val)){
+                            vis.add(n.val);
+                            q.add(n);                            
+                        }
+                    
                 }
-                else{
-                    Node n = new Node(p.n1.neighbors.get(i).val);
-                    l.add(n);
-                     q.add(new Pair(p.n1.neighbors.get(i),n));
-                    m.put(n.val, n);
-                }
+                Node newP = m.get(p.val);
+                newP.neighbors = l;
+                m.put(newP.val, newP);
             }
-            p.n2.neighbors = l;
-            // for(int i=0;i<p.n1.neighbors.size();i++){
-            //     if(!v.containsKey(p.n1.neighbors.get(i).val))
-            //     q.add(new Pair(p.n1.neighbors.get(i), p.n2.neighbors.get(i)));
-            // }
         }
-        return newNode;    
+        return newNode;
     }
     
 }
