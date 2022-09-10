0722ea31f9bff5ef738ae1a5679a47960ec99a2b
Sameer Ahmed <sameer7483@gmail.com>
2022-09-10 12:16:08 -0400

    Time: 16 ms (30.98%), Space: 46.8 MB (50.57%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,10 +1,10 @@
 class Solution {
     Boolean[] dp;
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = wordDict.stream().collect(Collectors.toSet());
         dp = new Boolean[s.length()];
+        Set<String> dict = wordDict.stream().collect(Collectors.toSet());
         return util(s, 0, dict);
-    }  
+    }
     
     public boolean util(String s, int start, Set<String> dict){
         if(start == s.length())
@@ -14,10 +14,9 @@ class Solution {
         for(int i=start;i<s.length();i++){
             if(dict.contains(s.substring(start, i+1))){
                 if(util(s, i+1, dict))
-                    return dp[start]= true;
+                    return dp[start] = true;
             }
         }
         return dp[start]=false;
     }
-    
 }
