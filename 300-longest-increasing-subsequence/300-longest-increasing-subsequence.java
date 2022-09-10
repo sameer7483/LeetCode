a7a3d0897ef136659521ad7b9d81b55b4673f9c5
Sameer Ahmed <sameer7483@gmail.com>
2022-09-10 13:02:25 -0400

    Time: 62 ms (64.39%), Space: 41.5 MB (98.85%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,13 +1,12 @@
 class Solution {
-    int[] dp;
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int[] lis = new int[n];
+        Arrays.fill(lis, 1);
         int res = 1;
-        for(int i=0;i<n;i++){
-            lis[i] = 1;
+        for(int i=1;i<n;i++){
             for(int j=i-1;j>=0;j--){
-                if(nums[i] > nums[j]){
+                if(nums[j] < nums[i]){
                     lis[i] = Math.max(lis[i], 1+lis[j]);
                 }
             }
@@ -15,35 +14,4 @@ class Solution {
         }
         return res;
     }
-    public int optimal(int nums[]){
-        int n= nums.length;
-        List<Integer> seq = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(seq.isEmpty() || nums[i] > seq.get(seq.size()-1)){
-                seq.add(nums[i]);
-            }
-            else{
-                int idx = binarySearch(seq, nums[i]);
-                seq.set(idx, nums[i]);
-            }
-        }
-        return seq.size();
-    }
-    
-    public int binarySearch(List<Integer> seq, int key){
-        int lo= 0, hi = seq.size()-1;
-        while(lo < hi){
-            int mid = lo+(hi-lo)/2;
-            if(seq.get(mid) < key){
-                lo = mid+1;
-            }
-            else if(seq.get(mid) > key)
-                hi = mid;
-            else
-                return mid;
-            
-        }
-        return lo;
-    }
-    
 }
