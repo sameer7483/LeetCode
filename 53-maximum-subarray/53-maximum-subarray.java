0ae83d5bc1eeeaae7f3cae9c6476371550b6441a
Sameer Ahmed <sameer7483@gmail.com>
2022-09-10 22:04:53 -0400

    Time: 2 ms (74.56%), Space: 73.5 MB (65.26%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,42 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //kadane
-        int max = Integer.MIN_VALUE/2;
-        int maxEndHere= 0;
+        int maxSum = Integer.MIN_VALUE;
+        int maxHere = 0;
         for(int i=0;i<nums.length;i++){
-            maxEndHere += nums[i];
-            max = Math.max(max, maxEndHere);
-            if(maxEndHere < 0){
-                maxEndHere = 0;
+            maxHere += nums[i];
+            maxSum = Math.max(maxSum, maxHere);
+            if(maxHere < 0){
+                maxHere = 0;
             }
         }
-        return max;
-        
-    }
-    
-    
-    // Divide and conquer
-    public int util(int []nums, int lo, int hi){
-        if(lo == hi)
-            return nums[lo];
-        int mid = lo+(hi-lo)/2;
-        int left = util(nums, lo, mid);
-        int right = util(nums, mid+1, hi);
-        int leftMax = Integer.MIN_VALUE;
-        int sum = 0;
-        for(int i=mid;i>=lo;i--){
-            sum += nums[i];
-            if(sum > leftMax)
-                leftMax = sum;
-        }
-        sum = 0;
-        int rightMax = Integer.MIN_VALUE;
-        for(int i=mid+1;i<=hi;i++){
-            sum += nums[i];
-            if(sum > rightMax)
-                rightMax = sum;
-        }
-        
-        return Math.max(leftMax+rightMax, Math.max(left, right));
+        return maxSum;
     }
 }
