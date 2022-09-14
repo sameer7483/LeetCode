38d1c2f0cb466019e3e54f7b6f71441ab6c2fa4b
Sameer Ahmed <sameer7483@gmail.com>
2022-09-14 17:25:14 -0400

    Time: 1055 ms (55.99%), Space: 225.2 MB (77.51%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -1,13 +1,10 @@
 class TrieNode{
-    TrieNode children[];
+    TrieNode[] children;
     boolean isEnd;
     public TrieNode(){
         children = new TrieNode[26];
-        isEnd = false;
     }
-    
 }
-
 class WordDictionary {
     TrieNode root;
     public WordDictionary() {
@@ -17,39 +14,36 @@ class WordDictionary {
     public void addWord(String word) {
         TrieNode ws = root;
         for(int i=0;i<word.length();i++){
-            if(ws.children[word.charAt(i)-'a'] == null)
-                ws.children[word.charAt(i)-'a'] = new TrieNode();
-            ws = ws.children[word.charAt(i)-'a'];
+            char c = word.charAt(i);
+            if(ws.children[c-'a']== null){
+                ws.children[c-'a'] = new TrieNode();
+            }
+            ws = ws.children[c-'a'];
         }
         ws.isEnd = true;
     }
     
     public boolean search(String word) {
-       return searchUtil(word, 0, root);
-            
+        return dfs(word, 0, root);
     }
-    public boolean searchUtil(String word,int start, TrieNode r){
-        if(r == null)
+    
+    public boolean dfs(String word, int start, TrieNode root){
+        if(root == null)
             return false;
-        if(start == word.length() && r.isEnd)
-            return true;
         if(start == word.length())
-            return false;
-
-        if(word.charAt(start) != '.'){
-            return searchUtil(word, start+1, r.children[word.charAt(start)-'a']);
+            return root.isEnd;
+        if(word.charAt(start) == '.'){
+            for(int i=0;i<26;i++){
+                if(dfs(word, start+1, root.children[i]))
+                    return true;
+            }
         }
         else{
-            for(int i=0;i<26;i++){
-                if(r.children[i] != null){
-                     if(searchUtil(word, start+1, r.children[i])){
-                        return true;
-                    }                   
-                }
-
-            }
-            return false;
+            if(root.children[word.charAt(start)-'a'] != null)
+                return dfs(word, start+1, root.children[word.charAt(start)-'a']);
+                
         }
+        return false;
     }
 }
 
