98c4405a9ab6314deedea24eb03fc4671b05bf96
Sameer Ahmed <sameer7483@gmail.com>
2022-09-15 14:10:06 -0400

    Time: 5 ms (70.12%), Space: 44.2 MB (85.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,26 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] cand, int target) {
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> path = new ArrayList<>();
-    util(cand,0,target, path, res);
-    return res;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        util(candidates, 0, target, new ArrayList<>(), res);
+        return res;
+        
     }
     
-    public void util(int[] cand, int start, int target, List<Integer> path, List<List<Integer>> res){
+    public void util(int[] cand, int start, int target,List<Integer> path, List<List<Integer>> res){
+        if(start==cand.length || target < 0)
+            return;
         if(target == 0){
+            
             res.add(new ArrayList<>(path));
-            return ;
+            return;
         }
-        for(int i=start;i<cand.length;i++){
-            if(cand[i] <= target){
-                path.add(cand[i]);
-                util(cand, i, target-cand[i],path, res);
-                path.remove(path.size()-1);
-            }
+        if(target-cand[start] >= 0){
+            path.add(cand[start]);
+            // System.out.println(path);
+            util(cand, start, target-cand[start], path, res);
+            path.remove(path.size()-1);
         }
+            util(cand, start+1, target, path, res);
     }
-
-    
-    
-
 }
