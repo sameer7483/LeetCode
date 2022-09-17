13b820f6e9dcd4c951ab27b6c253f1aac39908d5
Sameer Ahmed <sameer7483@gmail.com>
2022-09-17 15:47:40 -0400

    Time: 7 ms (8.46%), Space: 47.9 MB (29.93%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,41 +1,41 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] ni) {
-        int n = intervals.length;
+    public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = Arrays.stream(intervals).collect(Collectors.toList());
-        int i=0;
-        boolean flag =false;
-        for( i=0;i<intervals.length;i++){
-            if(res.get(i)[0]> ni[0]){
-                res.add(i, ni);
+        int i = 0;
+        int n = intervals.length;
+        boolean flag = false;
+        while(i< n){
+            if(intervals[i][0] > newInterval[0]){
+                res.add(i, newInterval);
                 flag = true;
                 break;
-            }
+            }   
+            i++;
         }
-        if(!flag)
-            res.add(ni);
-        //merge interval;
+        if(!flag){
+            res.add(newInterval);
+        }
+        
+        i = 0;
+        
         List<List<Integer>> ans = new ArrayList<>();
-        i=0;
         while(i < res.size()){
             int end = res.get(i)[1];
-            int j= i+1;
-            
-            while(j<res.size() && res.get(j)[0] <= end){
+            int j = i+1;
+            while(j< res.size() && res.get(j)[0] <= end){
                 end = Math.max(end, res.get(j)[1]);
-                
                 j++;
             }
-            
             ans.add(Arrays.asList(res.get(i)[0], end));
             i = j;
         }
-        int[][] ret = new int[ans.size()][2];
-        
+        int[][] inserted = new int[ans.size()][2];
         for( i=0;i<ans.size();i++){
-            ret[i][0] = ans.get(i).get(0);
-            ret[i][1] = ans.get(i).get(1);
+            inserted[i][0] = ans.get(i).get(0);
+            inserted[i][1] = ans.get(i).get(1);
         }
-        return ret;
+        return inserted;
         
     }
+    
 }
