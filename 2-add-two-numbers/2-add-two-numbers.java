623cefeba5091ec8116e3713311125aafbaa4eb9
Sameer Ahmed <sameer7483@gmail.com>
2022-09-21 09:55:28 -0400

    Time: 4 ms (41.98%), Space: 47.9 MB (47.97%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -14,64 +14,31 @@ class Solution {
             return l2;
         if(l2 == null)
             return l1;
-        int sum = 0;
+        int carry = 0;
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
         while(l1 != null && l2 != null){
-            sum += l1.val + l2.val;
-            temp.next = new ListNode(sum%10);
-            sum = sum/10;
+            temp.next = new ListNode((l1.val+l2.val+carry)%10);
+            carry = (l1.val+l2.val+carry)/10;
             temp = temp.next;
             l1 = l1.next;
             l2 = l2.next;
         }
         while(l1 != null){
-            sum += l1.val;
-            temp.next = new ListNode(sum%10);
-            sum = sum/10;
-            temp = temp.next;   
-             l1 = l1.next;
+            temp.next = new ListNode((l1.val+carry)%10);
+            carry = (l1.val+carry)/10;
+            temp = temp.next;
+            l1 = l1.next;            
         }
         while(l2 != null){
-            sum += l2.val;
-            temp.next = new ListNode(sum%10);
-            sum = sum/10;
-            temp = temp.next;   
-            l2 = l2.next;
+            temp.next = new ListNode((l2.val+carry)%10);
+            carry = (l2.val+carry)/10;
+            temp = temp.next;  
+            l2 = l2.next;            
         }
-        if(sum == 1)
-            temp.next = new ListNode(1);
+        if(carry > 0)
+            temp.next = new ListNode(carry);
         return dummy.next;
         
-    }
-    
-    public ListNode util(ListNode l1, ListNode l2, int rem){
-        if(l1 == null && rem == 0)
-            return l2;
-        if(l2 == null && rem == 0)
-            return l1;
-        if(l1 == null && l2 == null && rem ==1){
-            return new ListNode(1);
-        }
-        if(l1 == null && rem == 1){
-            int temp = l2.val;
-            l2.val = (temp+rem)%10;
-            rem = (temp+rem)/10;
-            l2.next = util(l1, l2.next, rem);
-            return l2;
-        }
-        
-        if(l2 == null && rem == 1){
-            int temp = l1.val;
-            l1.val = (temp+rem)%10;
-            rem = (temp+rem)/10;
-            l1.next = util(l1.next, l2, rem);
-            return l1;
-        }        
-        ListNode head = new ListNode();
-        head.val = (l1.val + l2.val + rem)%10;
-        rem = (l1.val + l2.val + rem)/10;
-        head.next = util(l1.next, l2.next, rem);
-        return head;
     }
 }
