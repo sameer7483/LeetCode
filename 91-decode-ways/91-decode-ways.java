0cc6f0741527d2ab53d5f70d516bc10195764391
Sameer Ahmed <sameer7483@gmail.com>
2022-10-01 10:48:46 -0400

    Time: 5 ms (14.94%), Space: 43.2 MB (8.72%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -4,28 +4,27 @@ class Solution {
     public int numDecodings(String s) {
         set = new HashSet<>();
         dp = new int[s.length()];
-        Arrays.fill(dp, -1); 
-        for(int i=1;i<=26;i++){
-            set.add(String.valueOf(i));    
-        }
-        return util(s,0);
+        Arrays.fill(dp, -1);
+        for(int i=1;i<=26;i++)
+            set.add(String.valueOf(i));
+        return util(s, 0);
     }
     
-    public int util(String s, int i){
-        if(i >= s.length())
+    public int util(String s, int start){
+        if(start >= s.length())
             return 1;
-        if(dp[i] != -1)
-            return dp[i];
-        int first = 0;
-        int second = 0;
-        if(set.contains(s.substring(i,i+1))){
-            first = util(s, i+1);
-        }
-        if(s.length()-i >= 2 && set.contains(s.substring(i,i+ 2))){
-            second = util(s, i+2);
-        }
-        return dp[i] = first + second;
+        if(dp[start] != -1)
+            return dp[start];
+        String one = s.substring(start, start+1);
+        String two = "";
+        if(start+2 <= s.length())
+            two = s.substring(start, start+2);
+        if(set.contains(one) && set.contains(two))
+            return dp[start] = util(s, start+1) + util(s, start+2);
+        else if(set.contains(one))
+            return dp[start]= util(s, start+1);
+        else
+            return dp[start] = 0;
     }
-    
     
 }
