5ab67e58fa3ebd964a3805fb733ebcb00c0fabf4
Sameer Ahmed <sameer7483@gmail.com>
2023-02-22 15:12:23 -0500

    Time: 30 ms (8.20%), Space: 77.4 MB (5.03%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for(int n : nums){
+            s.add(n);
+        }
+        return !(s.size() == nums.length);
+    }
+}
