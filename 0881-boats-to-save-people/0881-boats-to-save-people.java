8f4fb96b132d823314e2dd31fe00ba6362ba79af
Sameer Ahmed <sameer7483@gmail.com>
2023-04-03 09:49:30 -0400

    Time: 17 ms (98.31%), Space: 50.5 MB (69.91%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int count = 0;
+        int i=0, j= people.length-1;
+        Arrays.sort(people);
+        while(i<=j){
+            if(people[i]+people[j] <= limit){
+                i++;
+                j--;
+            }
+            else
+                j--;
+            count++;
+        }
+        return count;
+    }
+}
