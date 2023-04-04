a5b88fe1003ab669cd7ff2e5bda03a680387571c
Sameer Ahmed <sameer7483@gmail.com>
2023-04-04 10:36:02 -0400

    Time: 11 ms (85.92%), Space: 43.2 MB (53.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -2,34 +2,15 @@ class Solution {
     Map<String, Integer> m;
     public int partitionString(String s) {
         int res = 1;
-        Set<Character> set = new HashSet<>();
+        int bitmap = 0;
         for(int i=0;i<s.length();i++){
-            if(set.contains(s.charAt(i))){
+            if((bitmap & (1<<(s.charAt(i)-'a'))) > 0){
                 res++;
-                set = new HashSet<>();
-                set.add(s.charAt(i));
-                continue;
+                bitmap = 0;
             }
-            set.add(s.charAt(i));
+            bitmap = bitmap | (1<<(s.charAt(i)-'a'));
         } 
         return res;
-    }
-    
-    public int util(String s){
-        int res = Integer.MAX_VALUE/2;
-        if(s.length() == 0)
-            return 0;
-        if(m.containsKey(s))
-            return m.get(s);
-        Set<Character> set = new HashSet<>();
-        for(int i=0;i<s.length();i++){
-            if(set.contains(s.charAt(i)))
-                break;
-            set.add(s.charAt(i));
-            res = Math.min(res, 1+util(s.substring(i+1)));
-        }
-        m.put(s, res);
-        return res;        
     }
     
 }
