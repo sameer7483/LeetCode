f76a0ae652bc50509e1f6a42d39de706d76f7870
Sameer Ahmed <sameer7483@gmail.com>
2023-07-23 16:49:00 -0400

    Time: 17 ms (54.71%), Space: 43.4 MB (34.19%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,15 +3,20 @@ class Solution {
         int n = nums.length;
         int res = Integer.MAX_VALUE;
         int currMin = Integer.MAX_VALUE;
+        Arrays.sort(nums);
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(Math.abs(target-(nums[i]+nums[j]+nums[k])) < currMin){
-                        currMin = Math.abs(target-(nums[i]+nums[j]+nums[k]));
-                        res = nums[i]+nums[j]+nums[k];
-                    }
-                        
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
+                int currSum = nums[i] + nums[j] + nums[k];
+                if(Math.abs(currSum-target) < currMin){
+                    currMin = Math.abs(currSum-target);
+                    res = currSum;
                 }
+                if(currSum > target)
+                    k--;
+                else
+                    j++;
             }
         }
         return res;
