16703867f2ba61dc92ebd6592702a6afdf5d96ee
Sameer Ahmed <sameer7483@gmail.com>
2023-12-14 20:15:18 -0500

    Time: 43 ms (44.14%), Space: 16.4 MB (52.70%) - LeetHub

--- 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.py
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.py
@@ -3,20 +3,14 @@ class Solution:
         #[2, 3, 1, 4, 2, 5]
         if(len(nums) == 1):
             return 0
-        moveEven = 0
-        for i in range(0, len(nums), 2):
+        move = [0, 0]
+        for i in range(len(nums)):
             if i==0:
-                moveEven += max(nums[i]-nums[i+1]+1, 0)
+                move[i%2] += max(nums[i]-nums[i+1]+1, 0)
             elif i==len(nums)-1:
-                moveEven += max(nums[i]-nums[i-1]+1, 0)                
+                move[i%2] += max(nums[i]-nums[i-1]+1, 0)                
             else:
-                moveEven += max(nums[i] - min(nums[i-1], nums[i+1])+1, 0)
-        moveOdd = 0
-        for i in range(1, len(nums), 2):
-            if i==len(nums)-1:
-                moveOdd += max(nums[i]-nums[i-1]+1, 0)
-            else:
-                moveOdd += max(nums[i] - min(nums[i-1], nums[i+1])+1, 0)
-        return min(moveEven, moveOdd)
+                move[i%2] += max(nums[i] - min(nums[i-1], nums[i+1])+1, 0)
+        return min(move)
             
             
