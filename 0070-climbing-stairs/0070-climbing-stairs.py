bebb19ec5b7fc5ce7bd2964168fa33a0d6d57089
Sameer Ahmed <sameer7483@gmail.com>
2024-09-07 18:01:56 -0400

    Time: 42 ms (8.06%), Space: 16.5 MB (61.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        m = [-1] * (n+1)
+        @lru_cache(maxsize=None)
         def recurse(n):
             if n==1:
                 return 1
             if n==2:
                 return 2
-            if m[n] != -1:
-                return m[n]
-            m[n] = recurse(n-1) + recurse(n-2)
-            return m[n]
+            return recurse(n-1) + recurse(n-2)
         return recurse(n)
         
